206e165995d3578bdcd34691398a16f73faed64e
Tibolt <tibo.ak015@gmail.com>
2023-12-05 22:01:01 +0100

    cookie fix

--- src/routes/log-out/+page.server.ts
+++ src/routes/log-out/+page.server.ts
@@ -2,8 +2,10 @@ import { redirect } from "@sveltejs/kit";
 
 export const load = async (event) => {
   // remove the cookie
-  event.cookies.set("auth_token", "", {maxAge: 0});
-  event.cookies.set("storageID", "", {maxAge: 0});
+  // event.cookies.set("auth_token", "", {maxAge: 0});
+  event.cookies.delete("auth_token")
+  // event.cookies.set("storageID", "", {maxAge: 0});
+  event.cookies.delete("storageID")
 
   // redirect to the sign-in page
   throw redirect(301, "/login");
