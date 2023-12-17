8bc7b5b9f84d03cebc713b87f52c2eaa4caa4503
Tibolt <tibo.ak015@gmail.com>
2023-12-17 22:10:59 +0100

    Add sending email with password reset link

--- src/routes/forgot-password/+page.server.ts
+++ src/routes/forgot-password/+page.server.ts
@@ -4,6 +4,9 @@ import { isValidEmail } from '$lib/utils.js';
 import { fail } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import jwt from "jsonwebtoken";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API);
 
 export const actions = {
 	default: async (event) => {
@@ -27,8 +30,27 @@ export const actions = {
             const secret = process.env.JWT_SECRET + usr[0].password;
 
             const token = jwt.sign(payload, secret, {expiresIn: '30m'});
-            const link = `http://localhost:5173/forgot-password/${id}/${token}`;
+            const link = `https://grocerylistify.online/forgot-password/${id}/${token}`;
             console.log(link)
+
+            const emailMsg = {
+                to: usr[0].email,
+                from: {
+                    name: "Grocery Listify",
+                    email: "olekkrzak2001@gmail.com"
+                },
+                subject: "Grocery Listify Password Reset",
+                text: "Reset your password by clicking link below " + link,
+                html: `<p> Reset your password by clicking link below: </p><br/><a href="${link}">Reset Password</a>`
+            }
+
+            try {
+                sgMail.send(emailMsg);
+            } catch (err) {
+                console.log(err);
+                return fail(500, {message: 'email failed to send'});
+            }
+
             return {message: 'email sent'};
         }
 	}
